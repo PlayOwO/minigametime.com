a7926df150fafe877a1d8df46ec130c96f5b5851
PlayOwO <100187759+PlayOwO@users.noreply.github.com>
2022-10-28 18:53:22 +0800

    图片组件修复

--- components/Image.js
+++ components/Image.js
@@ -5,11 +5,11 @@
 // }
 import { getImageUrl } from "../lib/api";
 
-export default function Image({ className, title, alt, width, height, eager = false }) {
+export default function Image({ className, src, alt, width, height, eager = false }) {
 
   return <picture>
     <source type="image/avif" srcSet={getImageUrl(src, `avif`)} />
     <source type="image/webp" srcSet={getImageUrl(src, `webp`)} />
-    <img className={className} src={getImageUrl(title)} alt={alt ? alt : title} width={width ? width : 100} height={height ? height : 100} loading={eager ? `eager` : `lazy`} />
+    <img className={className} src={getImageUrl(src)} alt={alt} width={width ? width : 100} height={height ? height : 100} loading={eager ? `eager` : `lazy`} />
   </picture>
 }
